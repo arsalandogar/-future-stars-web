3245ac329655b9a6e2c1aff78a77296dab502024
arslandogar <arsalan.dogar@yahoo.com>
2025-12-12 18:10:27 +0500

    Add Modals and Notifications providers

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,10 +1,19 @@
 import type { ReactNode } from 'react';
 import { MantineProvider } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
+import { Notifications } from '@mantine/notifications';
 
 type AppProviderProps = {
   children: ReactNode;
 };
 
 export function AppProvider({ children }: AppProviderProps) {
-  return <MantineProvider>{children}</MantineProvider>;
+  return (
+    <MantineProvider>
+      <ModalsProvider>
+        <Notifications />
+        {children}
+      </ModalsProvider>
+    </MantineProvider>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
+import '@mantine/charts/styles.css';
 import '@/index.css';
 import { AppProvider } from '@/app/provider';
 import { AppRouter } from '@/app/router';
